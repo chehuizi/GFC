d21cc4d7c87ee5193b1ecdbd9a4f349eca515bcc
chehuizi <Chz@190972>
2021-06-19 20:35:07 +0800

    修改业务实体

--- bmf-base/src/main/java/com/chz/bmf/domain/base/entity/BusinessEntity4Domain.java
+++ bmf-base/src/main/java/com/chz/bmf/domain/base/entity/BusinessEntity4Domain.java
@@ -1,13 +1,26 @@
 package com.chz.bmf.domain.base.entity;
 
-import com.chz.bmf.domain.base.common.BusinessDomainNameEnum;
+import com.chz.bmf.domain.base.meta.BusinessDomainNameEnum;
 import lombok.Data;
 
 @Data
-public class BusinessEntity4Domain {
+public abstract class BusinessEntity4Domain {
 
     /**
      * 领域
      */
     private BusinessDomainNameEnum domainName;
+    /**
+     * 业务实体唯一ID
+     */
+    private String businessEntityId;
+
+    public BusinessEntity4Domain() {
+        this.businessEntityId = buildBusinessEntityId();
+    }
+
+    /**
+     * 构建业务实体唯一ID
+     */
+    public abstract String buildBusinessEntityId();
 }
--- bmf-domain-biz-ims/src/main/java/com/chz/bmf/domain/biz/ims/entity/ImsGoodsEntity.java
+++ bmf-domain-biz-ims/src/main/java/com/chz/bmf/domain/biz/ims/entity/ImsGoodsEntity.java
@@ -6,6 +6,17 @@ import lombok.Data;
 @Data
 public class ImsGoodsEntity extends BusinessEntity4Domain {
 
+    /**
+     * 货品SKU
+     */
     private String sku;
+    /**
+     * 货品SPU
+     */
     private String spu;
+
+    @Override
+    public String buildBusinessEntityId() {
+        return this.getSpu() + "_" + this.getSku();
+    }
 }
