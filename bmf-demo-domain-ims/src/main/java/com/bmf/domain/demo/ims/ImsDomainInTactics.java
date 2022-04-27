acbdebfe8e7757fb6d9a301d67c02c201e773f4d
chehuizi <chehuizi@didiglobal.com>
2022-04-27 22:15:52 +0800

    init

--- bmf-demo-domain-ims/src/main/java/com/bmf/domain/demo/ims/ImsDomainInTactics.java
+++
@@ -1,6 +0,0 @@
-package com.bmf.domain.demo.ims;
-
-import com.bmf.domain.base.tactics.BusinessDomainInTactics;
-
-public class ImsDomainInTactics extends BusinessDomainInTactics {
-}
--- bmf-demo-domain-ims/src/main/java/com/bmf/domain/demo/ims/service/ImsRetrievalService.java
+++
@@ -1,4 +0,0 @@
-package com.bmf.domain.demo.ims.service;
-
-public class ImsRetrievalService {
-}
