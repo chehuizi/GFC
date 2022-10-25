574171e9671b1152e801654c7d63eda675c14367
chehuizi <chehuizi@didiglobal.com>
2022-10-25 21:54:58 +0800

    init

--- bmf-common/src/main/java/com/bmf/common/aop/ApiAspect.java
+++ bmf-common/src/main/java/com/bmf/common/aop/ApiAspect.java
@@ -1,9 +1,26 @@
 package com.bmf.common.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
 /**
  * API拦截器
  */
-
+@Aspect
+@Component
 public class ApiAspect {
 
+    @Pointcut(value = "execution(* com.bmf.api..*.*(..))")
+    public void pointCut() {
+
+    }
+
+    @Around("pointCut()")
+    public Object invoke(ProceedingJoinPoint proceedingJoinPoint) {
+        String text = "";
+        return null;
+    }
 }
