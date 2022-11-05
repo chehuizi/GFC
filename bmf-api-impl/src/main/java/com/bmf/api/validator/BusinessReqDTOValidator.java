f0328e048d69781ae0c919d500ee8f2ab6c1964c
chehuizi <chehuizi@didiglobal.com>
2022-11-05 08:59:22 +0800

    init

--- bmf-api-impl/src/main/java/com/bmf/api/validator/BusinessReqDTOValidator.java
+++ bmf-api-impl/src/main/java/com/bmf/api/validator/BusinessReqDTOValidator.java
@@ -3,6 +3,7 @@ package com.bmf.api.validator;
 import com.bmf.api.business.BusinessReqDTO;
 import com.bmf.common.enums.BizCodeEnum;
 import com.bmf.common.exception.BizException;
+import com.bmf.common.utils.ParamCheckUtil;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -16,11 +17,8 @@ public class BusinessReqDTOValidator {
      * @return
      */
     public boolean v4Create(BusinessReqDTO businessReqDTO) {
-        if (Objects.isNull(businessReqDTO)) {
-            throw new BizException(BizCodeEnum.PARAM_ERROR);
-        } else if (Objects.isNull(businessReqDTO.getBusiness())) {
-            throw new BizException(BizCodeEnum.PARAM_ERROR, "business is null");
-        }
+        ParamCheckUtil.checkNull(businessReqDTO);
+        ParamCheckUtil.checkNull(businessReqDTO.getBusiness(), "business is null");
         return true;
     }
 
--- bmf-common/src/main/java/com/bmf/common/utils/ParamCheckUtil.java
+++ bmf-common/src/main/java/com/bmf/common/utils/ParamCheckUtil.java
@@ -1,4 +1,33 @@
 package com.bmf.common.utils;
 
+import com.bmf.common.enums.BizCodeEnum;
+import com.bmf.common.exception.BizException;
+
+import java.util.Objects;
+
+/**
+ * 参数校验工具类
+ */
 public class ParamCheckUtil {
+
+    /**
+     * 校验参数是否是空
+     * @param param
+     */
+    public static void checkNull(Object param) {
+        if (Objects.isNull(param)) {
+            throw new BizException(BizCodeEnum.PARAM_ERROR);
+        }
+    }
+
+    /**
+     * 校验参数是否是空，同时返回信息
+     * @param param
+     * @param message
+     */
+    public static void checkNull(Object param, String message) {
+        if (Objects.isNull(param)) {
+            throw new BizException(BizCodeEnum.PARAM_ERROR, message);
+        }
+    }
 }
