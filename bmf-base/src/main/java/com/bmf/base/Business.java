9543ae553fc69f440e52537f59f10911791e7ab5
chehuizi <chehuizi@didiglobal.com>
2022-12-08 23:06:09 +0800

    init

--- bmf-base/src/main/java/com/bmf/base/Business.java
+++ bmf-base/src/main/java/com/bmf/base/Business.java
@@ -1,5 +1,6 @@
 package com.bmf.base;
 
+import com.bmf.base.enums.BusinessPrefixEnum;
 import lombok.Data;
 
 import java.util.List;
@@ -11,23 +12,38 @@ import java.util.List;
 public class Business extends BaseModel {
 
     /**
+     * 业务前缀
+     * @see com.bmf.base.enums.BusinessPrefixEnum
+     */
+    private String businessPrefix;
+    /**
      * 业务编码
      */
     private Integer businessCode;
-
     /**
-     * 业务名称
+     * 业务名称（中文）
      */
     private String businessName;
-
+    /**
+     * 业务别名（英文）
+     */
+    private String businessAlias;
+    /**
+     * 业务描述
+     */
+    private String businessDesc;
     /**
      * 业务域列表
+     * @see BusinessDomain
      */
     private List<BusinessDomain> businessDomainList;
-
     /**
      * 业务域关系列表
+     * @see BusinessDomainRelation
      */
     private List<BusinessDomainRelation> businessDomainRelationList;
 
+    public Business() {
+        this.setBusinessPrefix(BusinessPrefixEnum.B.getPrefix());
+    }
 }
