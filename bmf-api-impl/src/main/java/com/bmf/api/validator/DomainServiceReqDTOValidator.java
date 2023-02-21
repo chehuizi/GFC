39d3df72a7f04247435594f0032c0839af684cf8
chehuizi <chehuizi@didiglobal.com>
2023-02-21 22:20:29 +0800

    init

--- bmf-api-impl/src/main/java/com/bmf/api/validator/DomainServiceReqDTOValidator.java
+++ bmf-api-impl/src/main/java/com/bmf/api/validator/DomainServiceReqDTOValidator.java
@@ -14,6 +14,12 @@ public class DomainServiceReqDTOValidator {
      */
     public boolean v4AddServiceAttr(DomainServiceReqDTO domainServiceReqDTO) {
         ParamCheckUtil.checkNull(domainServiceReqDTO, "param is null");
+        ParamCheckUtil.checkNull(domainServiceReqDTO.getDomainService(), "domain service is null");
+        ParamCheckUtil.checkNull(domainServiceReqDTO.getDomainService().getServiceCode(), "service code is null");
+        ParamCheckUtil.checkNull(domainServiceReqDTO.getServiceAttr(), "service attr is null");
+        ParamCheckUtil.checkBlank(domainServiceReqDTO.getServiceAttr().getAttrName(), "service attr name is blank");
+        ParamCheckUtil.checkBlank(domainServiceReqDTO.getServiceAttr().getAttrType(), "service attr type is blank");
+        ParamCheckUtil.checkBlank(domainServiceReqDTO.getServiceAttr().getAttrDesc(), "service attr desc is blank");
         return true;
     }
 
