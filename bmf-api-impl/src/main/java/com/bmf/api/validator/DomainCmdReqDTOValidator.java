d7043477ce8b15d6d5493a00834ad2379fca764f
chehuizi <chehuizi@didiglobal.com>
2023-02-27 00:05:27 +0800

    init

--- bmf-api-impl/src/main/java/com/bmf/api/validator/DomainCmdReqDTOValidator.java
+++ bmf-api-impl/src/main/java/com/bmf/api/validator/DomainCmdReqDTOValidator.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.Objects;
 
 @Service("domainReqDTOValidator")
-public class DomainReqDTOValidator {
+public class DomainCmdReqDTOValidator {
 
     /**
      * 校验queryOne方法
