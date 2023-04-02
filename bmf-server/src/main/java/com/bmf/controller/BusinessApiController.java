5b8afdaa486e1c11de75bd5acbf9a8e633c032ec
chehuizi <chehuizi@didiglobal.com>
2023-04-02 23:04:52 +0800

    init

---
+++ bmf-server/src/main/java/com/bmf/controller/BusinessApiController.java
@@ -0,0 +1,26 @@
+package com.bmf.controller;
+
+import com.bmf.api.Result;
+import com.bmf.api.application.BusinessApiCmdService;
+import com.bmf.api.application.dto.BusinessApiCmdReqDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "business/api")
+public class BusinessApiController {
+
+    private static final Logger logger = LoggerFactory.getLogger(BusinessApiController.class);
+
+    @Autowired
+    private BusinessApiCmdService businessApiCmdService;
+
+    @PostMapping("create")
+    public Boolean create(@RequestBody BusinessApiCmdReqDTO businessApiCmdReqDTO) {
+        Result<Boolean> result = businessApiCmdService.create(businessApiCmdReqDTO);
+        return result.getData();
+    }
+
+}
