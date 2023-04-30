6e686ce293c330852838cff242fa64d3486225c2
chehuizi <840739100@qq.com>
2023-04-30 09:46:41 +0800

    init

--- bmf-core/src/main/java/com/bmf/core/design/BusinessDomainDesign.java
+++
@@ -1,8 +0,0 @@
-package com.bmf.core.design;
-
-/**
- * 业务设计
- */
-public interface BusinessDomainDesign {
-
-}
---
+++ bmf-core/src/main/java/com/bmf/core/design/BusinessFlowDesign.java
@@ -0,0 +1,15 @@
+package com.bmf.core.design;
+
+import com.bmf.base.flow.BusinessFlow;
+
+/**
+ * 业务设计
+ */
+public interface BusinessFlowDesign {
+
+    /**
+     * 添加流程
+     * @return
+     */
+    boolean addFlow(BusinessFlow businessFlow);
+}
