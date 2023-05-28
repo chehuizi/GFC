aaa059044c7a082f994b9cddedc9a5d835090a04
jikeHero <549852856@qq.com>
2023-05-28 17:38:44 +0800

    feat: 文件调整

--- bmf-front/src/pages/domain/index.tsx
+++ bmf-front/src/pages/domain/index.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import { AndroidOutlined, MailOutlined } from "@ant-design/icons";
 import { Tabs } from "antd";
-import Strategy from "./strategy/index1";
+import Strategy from "./strategy/index";
 
 const Domain: React.FC = () => (
   <Tabs
