e6708418dd89a0664517ea670dd13fee2d519e81
chehuizi <840739100@qq.com>
2023-09-17 10:07:18 +0800

    init

--- bmf-design-api/src/main/java/com/bmf/api/application/dto/BusinessApiCmdReqDTO.java
+++ bmf-design-api/src/main/java/com/bmf/api/application/dto/BusinessApiCmdReqDTO.java
@@ -2,6 +2,7 @@ package com.bmf.api.application.dto;
 
 import com.bmf.api.ReqDTO;
 import com.bmf.base.application.DomainApi;
+import com.bmf.base.application.DomainApp;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -15,6 +16,10 @@ public class BusinessApiCmdReqDTO extends ReqDTO implements Serializable {
 
     private static final long serialVersionUID = -1577850349100928700L;
 
+    /**
+     * app
+     */
+    private DomainApp domainApp;
     /**
      * API列表
      */
