b05b3bb08eebacfb8f788cb2b8888998d1e94430
chehuizi <840739100@qq.com>
2023-10-18 23:15:29 +0800

    init

--- bmf-server/src/test/java/com/bmf/api/flow/BusinessFlowConfigCmdServiceTest.java
+++ bmf-server/src/test/java/com/bmf/api/flow/BusinessFlowConfigCmdServiceTest.java
@@ -4,9 +4,7 @@ import com.bmf.design.api.Result;
 import com.bmf.design.api.flow.BusinessFlowConfigCmdService;
 import com.bmf.design.api.flow.dto.BusinessFlowConfigCmdReqDTO;
 import com.bmf.design.base.Business;
-import com.bmf.design.base.enums.BusinessFlowNodeTypeEnum;
 import com.bmf.design.base.flow.BusinessFlow;
-import com.bmf.design.base.flow.BusinessFlowNode;
 import com.bmf.design.common.enums.ResultCodeEnum;
 import org.junit.Assert;
 import org.junit.Test;
@@ -38,21 +36,4 @@ public class BusinessFlowConfigCmdServiceTest {
         Assert.assertTrue(ResultCodeEnum.SUCCESS.getCode() == result.getCode());
     }
 
-    @Test
-    public void test_business_flow_add_node() {
-        BusinessFlowConfigCmdReqDTO businessFlowConfigCmdReqDTO = new BusinessFlowConfigCmdReqDTO();
-        BusinessFlow businessFlow = new BusinessFlow();
-        businessFlow.setBusinessCode(102);
-        businessFlow.setFlowId(1001);
-        businessFlowConfigCmdReqDTO.setBusinessFlow(businessFlow);
-        BusinessFlowNode flowNode = new BusinessFlowNode();
-        flowNode.setFlowId(1001);
-        flowNode.setNodeName("预约入库");
-        flowNode.setNodeAlias("apply_inbound");
-        flowNode.setNodeType(BusinessFlowNodeTypeEnum.DOMAIN.getType());
-        businessFlowConfigCmdReqDTO.setBusinessFlowNode(flowNode);
-        Result<Boolean> result = businessFlowCmdService.addFlowNode(businessFlowConfigCmdReqDTO);
-        System.out.println(result);
-        Assert.assertTrue(result.getData());
-    }
 }
